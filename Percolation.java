d1eb9b11b257e9d22c74acea3f8233d533419dab
Tan Wei Lian <weilian.tan@wizvision.local>
2018-03-22 17:52:16 +0800

    v1

--- Percolation.java
+++ Percolation.java
@@ -1,28 +1,127 @@
 public class Percolation {
     private int n;
-	private boolean[] sites;
+	private boolean[] isOpenSites;
+	private int[] sites;
+	private int[] weight;
+	private int openCount;
 	
     public Percolation(int n) {
 		this.n = n;
-		this.sites = new int[n*n];
+		this.openCount = 0;
+		this.sites = new int[n*n + 2];
+		this.isOpenSites = new boolean[n*n+2];
+		
+		for(int i = 0;i<isOpenSites.length;i++) {
+			isOpenSites[i] = false;
+		}
+		isOpenSites[0] = true;
+		isOpenSites[isOpenSites.length-1] = true;
 		for(int i = 0;i<sites.length;i++) {
-			this.sites[i] = false;
+			this.sites[i] = i;
 		}
 	}
 	
+	private void union(int item1, int item2) {
+		int root1 = root(item1);
+		int root2 = root(item2);
+		if(root1 == root2) {
+			//do nothing
+		} else {
+			int weight1 = weight[root1];
+			int weight2 = weight[root2];
+			
+			if(weight1 > weight2) {
+				sites[root2] = root1;
+				weight[root1] += weight2;
+			}else {
+				sites[root1] = root2;
+				weight[root2] += weight1;
+			}
+		
+		}
+	}
+	
+	private boolean find(int item1, int item2) {
+		int root1 = root(item1);
+		int root2 = root(item2);
+		
+		return root1 == root2;
+	}
+	
+	private int root(int item) {
+		
+		int current = item;
+		
+		while(sites[current] != current) {
+			current = sites[current];
+		}
+		
+		return current;
+	}
+	
 	private int convertIndex(int row, int col) {
-		return (row-1) * n + (col - 1);
+		return (row-1) * n + (col - 1) + 1;
 	}
 	
 	public void open(int row, int col) {
-		this.sites[convertIndex(row, col)] = true;
+		if(this.isOpen(row, col))
+			return ;
+		int index = convertIndex(row, col);
+		
+		this.isOpenSites[index] = true;
+		
+		//union up
+		if(row == 1) {
+			this.union(index, 0);
+		} else {
+			if(isOpen(row - 1, col)) {
+				this.union(index, convertIndex(row-1, col));
+			}
+		}
+		//union down
+		if(row == n) {
+			this.union(index, n*n+1);
+		} else {
+			if(isOpen(row+1, col)) {
+				this.union(index, convertIndex(row+1, col));
+			}
+		}
+		
+		//union left
+		if(col != 1) {
+			if(isOpen(row, col-1)) {
+				this.union(index, convertIndex(row, col-1));
+			}
+		}
+		
+		//union right
+		if(col != n) {
+			if(isOpen(row, col+1)) {
+				this.union(index, convertIndex(row, col+1));
+			}
+		}
+		openCount++;
+		
 	
 	}
-   public    void open(int row, int col)    // open site (row, col) if it is not open already
-   public boolean isOpen(int row, int col)  // is site (row, col) open?
-   public boolean isFull(int row, int col)  // is site (row, col) full?
-   public     int numberOfOpenSites()       // number of open sites
-   public boolean percolates()              // does the system percolate?
-
-   public static void main(String[] args)   // test client (optional)
+	
+	public boolean isOpen(int row, int col) {
+		return this.isOpenSites[convertIndex(row, col)];
+	}
+	
+    public boolean isFull(int row, int col) {
+        return this.openCount == n*n;
+    }
+	
+	public int numberOfOpenSites() {
+		return this.openCount;
+    }
+	
+	public boolean percolates() {
+		return this.find(0, n*n+1);
+	}
+	
+    public static void main(String[] args) {
+	
+	}
 }
--- PercolationStats.java
+++ PercolationStats.java
@@ -1,9 +1,21 @@
 public class PercolationStats {
-   public PercolationStats(int n, int trials)    // perform trials independent experiments on an n-by-n grid
-   public double mean()                          // sample mean of percolation threshold
-   public double stddev()                        // sample standard deviation of percolation threshold
-   public double confidenceLo()                  // low  endpoint of 95% confidence interval
-   public double confidenceHi()                  // high endpoint of 95% confidence interval
+	public PercolationStats(int n, int trials) {   // perform trials independent experiments on an n-by-n grid
+	}
 
-   public static void main(String[] args)        // test client (described below)
+	public double mean() {                         // sample mean of percolation threshold
+		return 0;
+	}
+	public double stddev() {                    // sample standard deviation of percolation threshold
+		return 0;
+	}
+	
+	public double confidenceLo() {                 // low  endpoint of 95% confidence interval
+		return 0;
+	}
+	public double confidenceHi() {                  // high endpoint of 95% confidence interval
+		return 0;
+	}
+
+	public static void main(String[] args) {        // test client (described below)
+	}
 }
